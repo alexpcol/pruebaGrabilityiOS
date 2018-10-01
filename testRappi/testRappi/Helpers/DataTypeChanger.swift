f01fc5f910cd150956113c502243b084403a9ed1
Chila <alexpc94@gmail.com>
2018-10-01 15:22:28 -0500

    dataype

--- testRappi/testRappi/Helpers/DataTypeChanger.swift
+++ testRappi/testRappi/Helpers/DataTypeChanger.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class DataTypeChanger: NSObject {
 
+    static func JSONDataToDiccionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+    
+    static func DictionaryToJSONData(jsonObject: AnyObject) throws -> String?
+    {
+        let data: NSData? = try? JSONSerialization.data(withJSONObject: jsonObject, options: JSONSerialization.WritingOptions.prettyPrinted) as NSData
+        
+        var jsonStr: String?
+        if data != nil {
+            jsonStr = String(data: data! as Data, encoding: String.Encoding.utf8)
+        }
+        
+        return jsonStr
+    }
 }
