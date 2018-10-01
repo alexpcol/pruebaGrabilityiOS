eae208edd17ae83cfedcacc85a87be223b59fbe8
Chila <alexpc94@gmail.com>
2018-10-01 15:22:18 -0500

    dataype changer

---
+++ testRappi/testRappi/Helpers/DataTypeChanger.swift
@@ -0,0 +1,13 @@
+//
+//  DataTypeChanger.swift
+//  testRappi
+//
+//  Created by chila on 10/1/18.
+//  Copyright © 2018 chila. All rights reserved.
+//
+
+import UIKit
+
+class DataTypeChanger: NSObject {
+
+}
