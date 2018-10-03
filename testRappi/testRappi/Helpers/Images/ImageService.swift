92055e5edb5508abfb730b173c5c7ba93d4be38b
mario <aperez@inflexionsoftware.com>
2018-10-03 12:04:59 -0500

    changed image downloader service

--- testRappi/testRappi/Helpers/Images/ImageService.swift
+++ testRappi/testRappi/Helpers/Images/ImageService.swift
@@ -11,6 +11,7 @@ import UIKit
 class ImageService: NSObject {
     
     static let cache = NSCache<NSString, UIImage>()
+    static var imageURLString: String?
     
     static func downloadImage(withURL url:URL, completion: @escaping (_ image:UIImage?)->()) {
          var downloadedImage:UIImage?
@@ -22,17 +23,22 @@ class ImageService: NSObject {
             }
             
             if let  downloadedimage = downloadedImage  {
+                if self.imageURLString == url.absoluteString
+                {
+                    DispatchQueue.main.async {
+                        completion(downloadedImage)
+                    }
+                }
                 cache.setObject(downloadedimage, forKey: url.absoluteString as NSString)
             }
             
-            DispatchQueue.main.async {
-                completion(downloadedImage)
-            }
+            
         }
         dataTask.resume()
     }
     
     static func getImage(withURL urlString:String, completion: @escaping (_ image:UIImage?)->()) {
+        imageURLString = urlString
         let url = URL(string: urlString)
         if let image = cache.object(forKey: urlString as NSString) {
             completion(image)
