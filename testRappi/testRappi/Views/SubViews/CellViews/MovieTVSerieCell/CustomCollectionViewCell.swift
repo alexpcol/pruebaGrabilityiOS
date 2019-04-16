f2c60bbac0c1fc1cb31a32335c651643323ac0e5
Alex <alexpc94@gmail.com>
2019-04-15 20:58:51 -0500

    fix: on tapped the status bar for scroll to top in collectionview

---
+++ testRappi/testRappi/Views/SubViews/CellViews/MovieTVSerieCell/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  testRappi
+//
+//  Created by chila on 4/15/19.
+//  Copyright © 2019 chila. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
